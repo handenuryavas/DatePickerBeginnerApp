590bd2f69bc85cf8abe8d3a4b73827c89bf67f14
handenuryavas <hande.nur038@gmail.com>
2023-01-30 13:53:29 +0300

    Date Picker

--- DatePickerBeginnerApp/ViewController.swift
+++ DatePickerBeginnerApp/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var birthdayField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        let datePicker = UIDatePicker()
+        datePicker.datePickerMode = .date
+        datePicker.addTarget(self, action: #selector(dateChange(datePicker:)), for:
+            UIControl.Event.valueChanged)
+        datePicker.preferredDatePickerStyle = .wheels
+        datePicker.maximumDate = Date()
+        
+        
+        birthdayField.inputView = datePicker
+        birthdayField.text =  formatDate(date: Date()) // todays Date
+    }
+    @objc func  dateChange(datePicker: UIDatePicker)
+    {
+     
+        birthdayField.text =  formatDate(date: datePicker.date)
+        
     }
 
-
+    func formatDate(date: Date) -> String
+    {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd. MM. yyyy"
+        return formatter.string(from: date)
+    }
 }
 
